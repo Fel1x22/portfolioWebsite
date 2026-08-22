bb9e527a091e1a12f40f8f5c4b7ee45702897810
n72019fu <felix.underwood@student.manchester.ac.uk>
2026-08-22 19:40:27 +0100

    Fix formatting

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,19 +1,24 @@
 import linkedin from "./assets/linkedin-svgrepo-com.svg";
-import github from "./assets/github-142-svgrepo-com.svg"
+import github from "./assets/github-142-svgrepo-com.svg";
 
 export default function Header() {
   return (
     <div className="header-container ">
       <div className="header-left">
         <h2>Felix's Portfolio</h2>
-        <a href="./assets/Felix_Underwood.pdf" download className="cv-download">CV Download</a>
+        <a href="./assets/Felix_Underwood.pdf" download className="cv-download">
+          CV Download
+        </a>
       </div>
       <div className="header-right">
         <a href="https://github.com/Fel1x22" className="social-box">
-          <img src={github} height="30px" width="30px"/>
+          <img src={github} height="30px" width="30px" />
         </a>
-        <a href="https://www.linkedin.com/in/felixunderwood/" className="social-box">
-          <img src={linkedin} height="30px" width="30px"/>
+        <a
+          href="https://www.linkedin.com/in/felixunderwood/"
+          className="social-box"
+        >
+          <img src={linkedin} height="30px" width="30px" />
         </a>
       </div>
     </div>
