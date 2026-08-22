30b69eeb69829f2d5fad5356f7436f7d616f7bbc
n72019fu <felix.underwood@student.manchester.ac.uk>
2026-08-22 19:38:16 +0100

    Improved cv download button

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,16 +1,20 @@
 import linkedin from "./assets/linkedin-svgrepo-com.svg";
+import github from "./assets/github-142-svgrepo-com.svg"
 
 export default function Header() {
   return (
     <div className="header-container ">
       <div className="header-left">
         <h2>Felix's Portfolio</h2>
-        <a>CV Download</a>
+        <a href="./assets/Felix_Underwood.pdf" download className="cv-download">CV Download</a>
       </div>
       <div className="header-right">
-        <div className="linkedin-logo">
-          <img src={linkedin} />
-        </div>
+        <a href="https://github.com/Fel1x22" className="social-box">
+          <img src={github} height="30px" width="30px"/>
+        </a>
+        <a href="https://www.linkedin.com/in/felixunderwood/" className="social-box">
+          <img src={linkedin} height="30px" width="30px"/>
+        </a>
       </div>
     </div>
   );
